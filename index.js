63e51b2a3c0d13357249a4d401e174904d5a7be1
sozascript <tesakungg23@gmail.com>
2026-08-31 11:33:42 +0700

    Change RichPresence name from 'soza' to 'ashyyy'

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on("ready", async () => {
   const r = new Discord.RichPresence(client)
     .setType("STREAMING")
     .setURL("https://www.twitch.tv/discord") // Must be a YouTube or Twitch stream link
-    .setName("soza")
+    .setName("ashyyy")
     .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1381140647322583195/1543838547885428766/copy_8BB7A35D-CD4F-4EDC-9353-8A2DFEC21544.gif?ex=6a965330&is=6a9501b0&hm=cc7c885106a4a77fc75c03265b039cc2cd801ac080494a584629bad636f89914&");
 
   client.user.setActivity(r);
